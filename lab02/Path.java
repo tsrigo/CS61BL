7b9e60af01bf40568f94dff9b759b18b7e3aa057
tsrigo <717660037@qq.com>
2022-06-29 10:04:05 +0800

    Finish Path.java.

--- lab02/Path.java
+++ lab02/Path.java
@@ -2,5 +2,39 @@
 public class Path {
 
     // TODO
+    private Point curr, next;
 
+    public Path(double x, double y){
+        this.next = new Point(x, y);
+        this.curr = new Point(x, x);
+    }
+
+    public double getCurrX(){
+        return this.curr.getX();
+    }
+
+    public double getCurrY(){
+        return this.curr.getY();
+    }
+
+    public double getNextX(){
+        return this.next.getX();
+    }
+
+    public double getNextY(){
+        return this.next.getY();
+    }
+
+    public Point getCurrentPoint(){
+        return this.curr;
+    }
+
+    public void setCurrentPoint(Point point){
+        this.curr = point;
+    }
+
+    public void iterate(double dx, double dy){
+        this.setCurrentPoint(this.next);
+        this.next = new Point(this.next.getX() + dx, this.next.getY() + dy);
+    }
 }
